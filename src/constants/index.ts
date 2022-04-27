559eec3533c6ab24a4679ea4b7995a9e9da61945
Nguyễn Dương <nguyenduong@Nguyens-MacBook-Pro.local>
2022-04-27 17:18:22 +0700

    fix: set default responsive

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,14 @@
 export const PREFIX_COMPONENT_CLASSNAME = 'jitera'
 export const PREFIX_ICON_CLASSNAME = 'jiteraicon'
+
+export const DEFAULT_RESPONSIVE_VISIBILITY = [
+  {
+    value: 'desktop'
+  },
+  {
+    value: 'tablet'
+  },
+  {
+    value: 'mobile'
+  }
+]
