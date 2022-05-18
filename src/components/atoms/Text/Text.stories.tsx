8fa1916576ec68754bc2b49b5a03af97fd823901
Jefry Dewangga <jefrydco@gmail.com>
2022-05-18 16:28:37 +0700

    feat(text): support link

--- src/components/atoms/Text/Text.stories.tsx
+++ src/components/atoms/Text/Text.stories.tsx
@@ -1,6 +1,6 @@
 import { Story } from '@storybook/react'
 
-import { Text as JiteraText, TextVariantType, TextProps } from './Text.component'
+import { Text as JiteraText, TextTypeEnum, TextProps } from './Text.component'
 
 const text = 'Jitera Automation Tool'
 
@@ -24,30 +24,14 @@ const TextTemplate: Story<TextProps> = (arguments_) => <JiteraText {...arguments
 export const Text = TextTemplate.bind({})
 Text.args = {
   ...defaultArguments,
-  variant: 'Text' as TextVariantType,
-  children: text,
-  type: 'danger'
-}
-
-export const Title = TextTemplate.bind({})
-Title.args = {
-  ...defaultArguments,
-  variant: 'Title' as TextVariantType,
-  level: 2,
+  textType: 'Text' as TextTypeEnum,
   children: text
 }
 
 export const Link = TextTemplate.bind({})
 Link.args = {
   ...defaultArguments,
-  variant: 'Link' as TextVariantType,
-  children: text
-}
-
-export const Paragraph = TextTemplate.bind({})
-Paragraph.args = {
-  ...defaultArguments,
-  variant: 'Paragraph' as TextVariantType,
+  textType: 'Link' as TextTypeEnum,
   children: text
 }
 
@@ -59,21 +43,7 @@ export default {
       description: 'Variant of the Text',
       control: {
         type: 'select',
-        options: ['Text', 'Title', 'Link', 'Paragraph']
-      }
-    },
-    type: {
-      description: 'Type of Text loading icon ',
-      control: {
-        type: 'select',
-        options: ['secondary', 'success', 'warning', 'danger', 'default']
-      }
-    },
-    level: {
-      description: 'Level(Size) of text',
-      control: {
-        type: 'select',
-        options: ['1', '2', '3', '4', '5']
+        options: ['Text', 'Link']
       }
     }
   }
