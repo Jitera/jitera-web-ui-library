40447746c840ef9ae36a35b3f283b1ff1aa7f9eb
Nguyễn Dương <nguyenduong@Nguyens-MacBook-Pro.local>
2022-05-22 20:01:39 +0700

    feat: improve icon button

--- src/components/atoms/Button/Button.component.tsx
+++ src/components/atoms/Button/Button.component.tsx
@@ -8,13 +8,22 @@ import { Icon, IconProps } from '@components/atoms/Icon/Icon.component'
 
 import { useResponsiveVisibility } from '@src/hooks/responsiveVisibility'
 
+import { TextWrapper } from './Button.styles'
+
 export interface ButtonProps extends PreviewProps, ComponentProps<Omit<AntButtonProps, 'type'>> {
   iconProps?: IconProps
   buttonType?: AntButtonProps['type']
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
-  const { buttonType = 'primary', responsiveVisibility, iconProps, className, ...rest } = props
+  const {
+    buttonType = 'primary',
+    responsiveVisibility,
+    iconProps,
+    className,
+    children,
+    ...rest
+  } = props
 
   const buttonIcon = useMemo(() => {
     if (!iconProps) {
@@ -27,7 +36,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => 
   const { classNames } = useResponsiveVisibility({ className, responsiveVisibility })
 
   return (
-    <AntButton type={buttonType} icon={buttonIcon} {...rest} className={classNames} ref={ref} />
+    <AntButton type={buttonType} icon={buttonIcon} {...rest} className={classNames} ref={ref}>
+      <TextWrapper hasIcon={!!buttonIcon}>{children}</TextWrapper>
+    </AntButton>
   )
 })
 
